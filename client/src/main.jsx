df3a4cd5b3f322eda45898da5618d293c009bdf7
JoaoVictorEmcompre <cadastro2@emcompre.com.br>
2025-08-07 13:01:34 -0300

    feat/ajuste dos logs

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import * as Sentry from "@sentry/react";
-import { BrowserTracing } from "@sentry/tracing";
+import { BrowserTracing } from "@sentry/react";
 
 Sentry.init({
   dsn: "https://6ca5482f9d9e34e42b416d957c608082@o4509803577147392.ingest.us.sentry.io/4509803577999360",
